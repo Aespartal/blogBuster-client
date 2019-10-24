c95c95059b94d1e41413b49884f824f16e4cae9b
Aespartal <36635622+Aespartal@users.noreply.github.com>
2019-10-25 00:12:53 +0200

    Nuevo post (Inacabado)

--- app/post/new/new.js
+++ app/post/new/new.js
@@ -1,13 +1,18 @@
 var miControlador = miModulo.controller(
     "postNewController",
     ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
-        // $http({
-        //     method: 'GET',
-        //     url: 'http://localhost:8081/blogbuster/json?ob=post&op=get&id=' + $routeParams.id
-        // }).then(function (response) {
-        //     $scope.status = response.data.status;
-        //     $scope.traerpost = response.data.response;
-        // }, function () {
-        // })
+
+       
+
+        $scope.new = function(data) {
+            data = {data:$scope.post}
+            $http.post("http://localhost:8081/blogbuster/json?ob=post&op=insert", data)
+         .then(function successCallback(response) {
+             $scope.status = response.data.status;
+            alert($scope.status);
+         }, function errorCallback(response) {
+            alert("No funciona");
+         })
+        }    
     }]
 )
