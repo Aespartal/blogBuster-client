05a6e42e46c4fbad8585cb9b09e1d8d10faa20b0
ESPARTAL MOROLLON  ALEJANDRO <a035595797p@stretch-builder>
2019-10-25 11:23:01 +0200

    Navegador include

--- app/post/edit/edit.js
+++ app/post/edit/edit.js
@@ -1,6 +1,7 @@
 var miControlador = miModulo.controller(
     "postEditController",
     ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+
         $http({
             method: 'GET',
             url: 'http://localhost:8081/blogbuster/json?ob=post&op=get&id=' + $routeParams.id
--- app/post/new/new.js
+++ app/post/new/new.js
@@ -1,6 +1,7 @@
 var miControlador = miModulo.controller(
     "postNewController",
     ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+        $scope.sitio = "new";
         $scope.new = function(data) {
             data = {data:$scope.post}
             $http.post("http://localhost:8081/blogbuster/json?ob=post&op=insert", data)
